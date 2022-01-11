893b2f2198dd0bb2a96e7dd9890d6cc23d28b91f
Diego Anibal Dalairac <5948252+ddalairac@users.noreply.github.com>
2022-01-11 16:54:13 -0300

    comp name

--- demo/src/router/index.ts
+++ demo/src/router/index.ts
@@ -13,11 +13,8 @@ const routes: Array<RouteConfig> = [
   {
     path: "/wc",
     name: "WCView",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/WCView.vue"),
+      import(/* webpackChunkName: "WCView" */ "../views/WCView.vue"),
   },
 ];
 
