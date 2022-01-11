d75f6e7da075c66d34b85f8d0f9ccce1969a07c5
Diego Anibal Dalairac <5948252+ddalairac@users.noreply.github.com>
2022-01-11 18:01:49 -0300

    build test

--- docs/wc-lib-async/wc.js
+++ docs/wc-lib-async/wc.js
@@ -8429,7 +8429,7 @@ __webpack_require__.r(__webpack_exports__);
 
 if (typeof window !== 'undefined') {
   var currentScript = window.document.currentScript
-  if (Object({"NODE_ENV":"production","BASE_URL":"/"}).NEED_CURRENTSCRIPT_POLYFILL) {
+  if (Object({"NODE_ENV":"production","BASE_URL":"/poc-wc-lib-async/"}).NEED_CURRENTSCRIPT_POLYFILL) {
     var getCurrentScript = __webpack_require__("8875")
     currentScript = getCurrentScript()
 
