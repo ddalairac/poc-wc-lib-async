1a1cc812626d95ca1e21e6ad68bf2b025a037aef
Diego Anibal Dalairac <5948252+ddalairac@users.noreply.github.com>
2022-01-11 16:23:32 -0300

    add builds for github pages

--- wc-lib/src/App.vue
+++ wc-lib/src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <VButton>Default</VButton>
-    <VButton variant="btn-secondary">Secondary</VButton>
-    <VButton variant="btn-outline-secondary">Outline Secondary</VButton>
-    <VDropdown text="Acctions Dropdown" :items="items" @clicked="onClickedItem"/>
-    <VDropdown text="Acctions Dropdown 2" :items="items" variant="btn-secondary" @on-click="onClickedItem">
-      <li class="dropdown-item" @click="onClickedItem('Manual Action')">Manual Action</li>
-      <li class="dropdown-item" @click="onClickedItem('Manual Action 2')">Manual Action 2</li>
-    </VDropdown>
+    <div class="components">
+      <VButton>Default</VButton>
+      <VButton variant="btn-secondary">Secondary</VButton>
+      <VButton variant="btn-outline-secondary">Outline Secondary</VButton>
+    </div>
+    <div class="components">∫
+      <VDropdown
+        text="Acctions Dropdown"
+        :items="items"
+        @clicked="onClickedItem"
+      />
+      <VDropdown
+        text="Acctions Dropdown 2"
+        :items="items"
+        variant="btn-secondary"
+        @on-click="onClickedItem"
+      >
+        <li class="dropdown-item" @click="onClickedItem('Manual Action')">
+          Manual Action
+        </li>
+        <li class="dropdown-item" @click="onClickedItem('Manual Action 2')">
+          Manual Action 2
+        </li>
+      </VDropdown>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import VButton from "./components/VButton.vue";
-import VDropdown, { tViewValue } from "./components/VDropdown.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import VButton from './components/VButton.vue';
+import VDropdown, { tViewValue } from './components/VDropdown.vue';
 
 @Component({
   components: {
@@ -26,16 +43,16 @@ import VDropdown, { tViewValue } from "./components/VDropdown.vue";
 export default class App extends Vue {
   items: tViewValue[] = [
     {
-      view: "Action 1",
-      value: "action1",
+      view: 'Action 1',
+      value: 'action1',
     },
     {
-      view: "Action 2",
+      view: 'Action 2',
       value: false,
     },
     {
-      view: "Action 3",
-      value: ["action3", true],
+      view: 'Action 3',
+      value: ['action3', true],
     },
   ];
   onClickedItem(item: tViewValue) {
@@ -52,5 +69,15 @@ export default class App extends Vue {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .components {
+    display: flex;
+    border-top: 1px solid #ccc;
+    padding: 30px 0 0;
+    margin: 30px;
+    > * {
+      margin-right: 10px;
+    }
+  }
 }
 </style>
