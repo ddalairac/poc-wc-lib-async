a6eeda639de05af86955d35acfda83214cafe3de
Lucas Zosi <lzosi@perceptyx.com>
2022-03-09 12:21:40 -0300

    Added some pocs

--- wc-vainilla/employee-card/employee-card.js
+++ wc-vainilla/employee-card/employee-card.js
@@ -20,18 +20,64 @@ template.innerHTML = `
 
 class EmployeeCard extends HTMLElement{
   constructor(){
-      super();
-      const { name, avatar } = this.data; // MAGIA ACÄ (?)
-      this.attachShadow({ mode: 'open'});
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
-      this.shadowRoot.querySelector('h3').innerText = name;
-      this.shadowRoot.querySelector('img').src = avatar;   
-  } 
-
-  connectedCallback() {
+    super();
+    this.attachShadow({ mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.render(this.data);
   }
 
-  render(){
+  render(data) {
+    const { name, avatar } = this.data;
+    this.shadowRoot.querySelector('h3').innerText = name;
+    this.shadowRoot.querySelector('img').src = avatar;   
   }
 }
+
+class EmployeeCardHook extends HTMLElement{
+  constructor() {
+    console.log('constructor');
+    super();
+    this.attachShadow({ mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+    this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+  }
+
+  connectedCallback() {
+    console.log('connectedCallback');
+  }
+
+  static get observedAttributes() {
+    return ['data', 'onUpdate'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+     return;
+   }
+    console.log(`The attribute ${name} has changed`);
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  set data(newVal) {
+    this._data = newVal;
+    const { name, avatar } = this.data;
+    this.shadowRoot.querySelector('h3').innerText = name;
+    this.shadowRoot.querySelector('img').src = avatar;   
+    this.onUpdate();
+  }
+
+  get onUpdate() {
+    return this._onUpdate;
+  }
+
+  set onUpdate(newVal) {
+    this._onUpdate = newVal;
+  }
+}
+
 window.customElements.define('employee-card', EmployeeCard);
+window.customElements.define('employee-card-hook', EmployeeCardHook);
